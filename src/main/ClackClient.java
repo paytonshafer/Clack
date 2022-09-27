c82b33cc6884461d422598a5005a4b6870ef31ad
paytonshafer <paytonshafer@gmail.com>
2022-09-27 09:02:53 -0400

    added variables

--- src/main/ClackClient.java
+++ src/main/ClackClient.java
@@ -1,4 +1,13 @@
 package main;
 
+import data.ClackData;
+
 public class ClackClient {
+    private String username;
+    private String hostName;
+    private int port;
+    private boolean closeConnection;
+    private ClackData dataToSendToServer;
+    private ClackData dataToReceiveFromServer;
+
 }
--- src/main/ClackServer.java
+++ src/main/ClackServer.java
@@ -1,4 +1,10 @@
 package main;
 
+import data.ClackData;
+
 public class ClackServer {
+    private int port;
+    private boolean closeConnection;
+    private ClackData dataToSendToClient;
+    private ClackData dataToReceiveFromClient;
 }

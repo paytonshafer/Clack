5a0ddcd5a21576592b891e1be182f1339ea4592c
paytonshafer <paytonshafer@gmail.com>
2022-09-27 20:44:38 -0400

    added functions

--- src/main/ClackClient.java
+++ src/main/ClackClient.java
@@ -9,7 +9,7 @@ public class ClackClient {
     private boolean closeConnection;
     private ClackData dataToSendToServer;
     private ClackData dataToReceiveFromServer;
-    static final int DEAFAUT_PORT_NUMBER = 7000;
+    static final int DEFAULT_PORT_NUMBER = 7000;
 
     public ClackClient(String u, String h, int p){
         this.userName = u;
@@ -21,7 +21,7 @@ public class ClackClient {
     }
 
     public ClackClient(String u, String h){
-        this(u,h,DEAFAUT_PORT_NUMBER);
+        this(u,h,DEFAULT_PORT_NUMBER);
     }
 
     public ClackClient(String u){
@@ -42,15 +42,27 @@ public class ClackClient {
 
     public void printData(){}
 
-    public String getUserName(){
-        return userName;
+    public String getUserName(){return userName;}
+
+    public String getHostName(){return hostName;}
+
+    public int getPort(){return port;}
+
+    public int hashCode(){return userName.length()*hostName.length()*port;}
+
+    public boolean equals(Object obj){
+        ClackClient other = (ClackClient) obj;
+        return this.hostName.equals(other.hostName) && this.userName.equals(other.userName) &&
+                this.closeConnection == other.closeConnection && this.port == other.port &&
+                 this.dataToSendToServer.equals(other.dataToSendToServer) &&
+                  this.dataToReceiveFromServer.equals(other.dataToReceiveFromServer);
     }
 
-    public String getHostName(){
-        return hostName;
-    }
-
-    public int getPort(){
-        return port;
+    public String toString(){
+        return "The client's username is" + this.userName + ". The current host Name is " +
+                this.hostName + ". The current port number is " + this.port +
+                 ". The current status of the connection being closed is " + this.closeConnection +
+                  ". The data sent to the server is " + this.dataToSendToServer +
+                   ". The data received from the server is " + this.dataToReceiveFromServer + ".";
     }
 }

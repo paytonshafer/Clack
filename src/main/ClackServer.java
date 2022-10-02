0d31beb395efc7aa2c4dcfc4cf80e1b01ca7d76d
paytonshafer <paytonshafer@gmail.com>
2022-10-02 19:11:11 -0400

    added javadoc comments

--- src/main/ClackServer.java
+++ src/main/ClackServer.java
@@ -2,6 +2,10 @@ package main;
 
 import data.ClackData;
 
+/**
+ * The ClackServer class represents the server that clients connect to
+ * This class holds data for the server
+ */
 public class ClackServer {
     private int port;
     private boolean closeConnection;
@@ -9,6 +13,10 @@ public class ClackServer {
     private ClackData dataToReceiveFromClient;
     static final int DEFAULT_PORT_NUMBER = 7000;
 
+    /**
+     * This is a constructor for the ClackServer class
+     * @param p This is the port number a client connects to
+     */
     public ClackServer(int p){
         this.port = p;
         this.closeConnection = false;
@@ -16,6 +24,9 @@ public class ClackServer {
         this.dataToReceiveFromClient = null;
     }
 
+    /**
+     * This is the default constructor for the ClackServer class
+     */
     public ClackServer(){
         this(DEFAULT_PORT_NUMBER);
     }
@@ -26,15 +37,32 @@ public class ClackServer {
 
     public void sentData(){}
 
+    /**
+     * This functions returns the port number
+     * @return This returns the port number
+     */
     public int getPort() {return port;}
 
+    /**
+     * This is a function to override hash code for the ClackServer object
+     * @return This returns a hash code such that equal objects have the same hash code
+     */
     public int hashCode(){return port;}
 
+    /**
+     * This is a function to override the equals function for the ClackServer object
+     * @param obj This represents the other object the server is being compared to
+     * @return This returns true or false for whether the servers are equal
+     */
     public boolean equals(Object obj) {
         ClackServer other = (ClackServer) obj;
         return this.port == other.port;
     }
 
+    /**
+     * This is a function to override the toString function for the ClackServer object
+     * @return This returns the server as a string such that it can be printed out
+     */
     public String toString(){
         return "The current port number is " + this.port +
                 ". The current status of the connection being closed is " + this.closeConnection +

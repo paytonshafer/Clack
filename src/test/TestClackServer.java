9bd1727460567f7907127a99a858eb67c4da03d5
paytonshafer <paytonshafer@gmail.com>
2022-11-12 11:03:01 -0500

    wrote main for ClackServer

--- src/test/TestClackServer.java
+++ src/test/TestClackServer.java
@@ -1,29 +1,17 @@
 package test;
 
 import main.ClackServer;
+import java.io.IOException;
+
 
 public class TestClackServer {
-    public static void main(String args[]) {
-        ClackServer server1 = new ClackServer(22);
-        ClackServer server2 = new ClackServer(22);
-        ClackServer server3 = new ClackServer();
-
-        System.out.println("The port humber is " + server1.getPort());
-
-        if (server1.equals(server2))
-            System.out.println("server 1 and server 2 are equal");
-        else
-            System.out.println("server 1 and server 2 are not equal");
-
-        if (server1.equals(server3))
-            System.out.println("server 1 and server 3 are equal");
-        else
-            System.out.println("server 1 and server 3 are not equal");
-
-        System.out.println("The hash code of server 1 is " + server1.hashCode());
-        System.out.println("The hash code of server 2 is " + server2.hashCode());
-        System.out.println("The hash code of server 3 is " + server3.hashCode());
-
-        System.out.println(server1);
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        if(args.length == 0) {
+            ClackServer defaultServer = new ClackServer();
+            defaultServer.start();
+        }else{
+            ClackServer server = new ClackServer(Integer.parseInt(args[0]));
+            server.start();
+        }
     }
 }

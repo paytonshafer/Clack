d0100df155e0dfbe3ec52e3107554aa0b23dce93
paytonshafer <paytonshafer@gmail.com>
2022-11-14 09:08:04 -0500

    added javadoc

--- src/test/TestClackServer.java
+++ src/test/TestClackServer.java
@@ -3,7 +3,19 @@ package test;
 import main.ClackServer;
 import java.io.IOException;
 
-
+/**
+ * This is a main method for ClackServer.
+ *
+ * If ClackServer is called with no arguments it creates a
+ * connection with the default port number
+ *
+ * If ClackServer is called with an argument, it will take
+ * the information from the argument and put the information
+ * into the constructor of the server. The argument syntax is
+ * as follows: <PORT>
+ *
+ * @param args
+ */
 public class TestClackServer {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         if(args.length == 0) {
